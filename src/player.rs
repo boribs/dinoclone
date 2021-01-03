6fd4831bd103d481d1b5fe415bceea3702d567d0
Bulagro <christumer@gmail.com>
2021-01-02 20:38:04 -0600

    Player colors

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,7 @@
-use ncurses::mvaddch;
+use ncurses::{attroff, attron, mvaddch, COLOR_PAIR};
 
 use super::terrain as t;
+use super::colors as c;
 
 const PLAYER_CHAR: u32 = '$' as u32;
 const JUMP_TO_MAX_HEIGHT_DIST: i32 = 3;
@@ -89,5 +90,7 @@ impl Player {
 }
 
 pub fn draw_player(p: &Player, px: i32) {
+    attron(COLOR_PAIR(c::PAIR_RED));
     mvaddch(p.y_pos, px, PLAYER_CHAR);
+    attroff(COLOR_PAIR(c::PAIR_RED));
 }
